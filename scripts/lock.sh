263ad7a8adf8f6bbd8f7e86b3e3a3ed5e14ff0a0
Matt Kline <matt@bitbashing.io>
2015-12-15 09:51:31 -0800

    Make lock.sh quieter

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -8,6 +8,6 @@ umask 066
 scrot "$ss"
 # Use ffmpeg instead of convert/mogrify because it uses these crazy things
 # called threads (and gives you finer control over the scaling).
-ffmpeg -y -i "$ss" -vf "scale=0.1*in_w:-1:flags=area,scale=10*in_w:-1:flags=neighbor" "$bg"
+ffmpeg -y -v error -i "$ss" -vf "scale=0.1*in_w:-1:flags=area,scale=10*in_w:-1:flags=neighbor" "$bg"
 i3lock -i "$bg"
-rm -v "$ss" "$bg"
+rm "$ss" "$bg"
