8af82b4b1d8526b067b3b8c5711f6a46c59bfa86
Matt Kline <matt@bitbashing.io>
2016-06-17 11:52:10 -0700

    Move work monitor scripts to work-scripts/

--- scripts/desk-monitors.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output LVDS-0 --off --output DP-0 --pos 0x0 --auto --output DP-2 --auto --right-of DP-0 --output VGA-0 --auto --right-of DP-2
--- scripts/laptop-screen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --off --output DP-2 --off --output VGA-0 --off --output LVDS-0 --auto
